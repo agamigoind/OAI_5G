4e47ed3678935d653d769e87c203aa194e01701f
Raghavendra Dinavahi <raghavendra.dinavahi@iis.fraunhofer.de>
2025-04-10 18:28:36 +0200

    ulsyncvalidDuration in conf file is in seconds instead of enum

--- openair2/GNB_APP/gnb_config.c
+++ openair2/GNB_APP/gnb_config.c
@@ -467,6 +467,40 @@ static void fix_tdd_pattern(NR_ServingCellConfigCommon_t *scc)
   }
 }
 
+static int get_ulsyncvalidityduration_enum_value(int val)
+{
+  int retval = -1;
+
+  switch (val) {
+    case 5:
+    case 10:
+    case 15:
+    case 20:
+    case 25:
+    case 30:
+    case 35:
+    case 40:
+    case 45:
+    case 50:
+    case 55:
+      retval = val/5 - 1;
+      break;
+    case 60:
+    case 120:
+    case 180:
+    case 240:
+      retval = 10 + val/60;
+      break;
+    case 900:
+      retval = 15;
+      break;
+    default:
+      AssertFatal(1==0,"ulsyncvalidityDuration in SIB19 set to invalid value in Conf file\n");
+      break;
+  }
+  return retval;
+}
+
 void fix_scc(NR_ServingCellConfigCommon_t *scc, uint64_t ssbmap)
 {
   scc->ssb_PositionsInBurst->present = get_ssb_len(scc);
@@ -568,6 +602,9 @@ void fix_scc(NR_ServingCellConfigCommon_t *scc, uint64_t ssbmap)
   if (*scc->ext2->ntn_Config_r17->ntn_UlSyncValidityDuration_r17 == 0) {
     free(scc->ext2->ntn_Config_r17->ntn_UlSyncValidityDuration_r17);
     scc->ext2->ntn_Config_r17->ntn_UlSyncValidityDuration_r17 = NULL;
+  } else {
+    int val = get_ulsyncvalidityduration_enum_value(*scc->ext2->ntn_Config_r17->ntn_UlSyncValidityDuration_r17);
+    *scc->ext2->ntn_Config_r17->ntn_UlSyncValidityDuration_r17 = val;
   }
   if (*scc->ext2->ntn_Config_r17->cellSpecificKoffset_r17 == 0) {
     free(scc->ext2->ntn_Config_r17->cellSpecificKoffset_r17);
