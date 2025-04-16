arandomdev/openairinterface5g

Handle NGAP Handover Command on source gNB
* add NG Handover Command message decoder
* process in RRC: encode RRCReconfiguration message
  from the received HandoverCommandMessage
* trigger RRCReconfiguration for handover

Co-authored-by: batuhan duyuler <batuhan.duyuler@firecell.io>

openair2/COMMON/ngap_messages_types.h
@@ -69,6 +69,7 @@
 #define NGAP_HANDOVER_FAILURE(mSGpTR) (mSGpTR)->ittiMsg.ngap_handover_failure
 #define NGAP_HANDOVER_REQUEST(mSGpTR) (mSGpTR)->ittiMsg.ngap_handover_request
 #define NGAP_HANDOVER_REQUEST_ACKNOWLEDGE(mSGpTR) (mSGpTR)->ittiMsg.ngap_handover_request_ack
+#define NGAP_HANDOVER_COMMAND(mSGpTR) (mSGpTR)->ittiMsg.ngap_handover_command
 
 #define NGAP_UE_CONTEXT_RELEASE_REQ(mSGpTR)     (mSGpTR)->ittiMsg.ngap_ue_release_req
 #define NGAP_PDUSESSION_RELEASE_COMMAND(mSGpTR)      (mSGpTR)->ittiMsg.ngap_pdusession_release_command
@@ -701,6 +702,37 @@ typedef struct {
   // Target to Source Transparent Container
   ngap_pdu_t target2source;
 } ngap_handover_request_ack_t;
+
+/* 9.3.4.10 3GPP TS 38.413 */
+typedef struct {
+  // QoS Flow to be Forwarded List
+  uint8_t nb_of_qos_flow;
+  pdusession_associate_qosflow_t qos_setup_list[QOSFLOW_MAX_VALUE];
+  // UL Forwarding UP TNL Information
+  uint32_t gtp_teid;
+  transport_layer_addr_t gNB_addr;
+} ho_command_transfer_t;
+
+typedef struct {
+  // PDU Session ID (M)
+  uint8_t pdusession_id;
+  // Handover Command Transfer (M)
+  ho_command_transfer_t ho_command_transfer;
+} pdusession_resource_handover_t;
+
+typedef struct {
+  // RAN UE NGAP ID
+  uint32_t gNB_ue_ngap_id;
+  // AMF UE NGAP ID
+  uint64_t amf_ue_ngap_id;
+  // Target to Source Transparent Container
+  ngap_pdu_t handoverCommand;
+  // Handover Type
+  ho_type_t handoverType;
+  // PDU Session Resource Handover List
+  uint8_t nb_of_pdusessions;
+  pdusession_resource_handover_t pdu_sessions[NGAP_MAX_PDUSESSION];
+} ngap_handover_command_t;
 
 typedef struct ngap_ue_cap_info_ind_s {
   uint32_t  gNB_ue_ngap_id;

agamigoind/OAI_5G

openair2/COMMON/ngap_messages_types.h
@@ -69,6 +69,7 @@
 #define NGAP_HANDOVER_FAILURE(mSGpTR) (mSGpTR)->ittiMsg.ngap_handover_failure
 #define NGAP_HANDOVER_REQUEST(mSGpTR) (mSGpTR)->ittiMsg.ngap_handover_request
 #define NGAP_HANDOVER_REQUEST_ACKNOWLEDGE(mSGpTR) (mSGpTR)->ittiMsg.ngap_handover_request_ack
+#define NGAP_HANDOVER_COMMAND(mSGpTR) (mSGpTR)->ittiMsg.ngap_handover_command
 
 #define NGAP_UE_CONTEXT_RELEASE_REQ(mSGpTR)     (mSGpTR)->ittiMsg.ngap_ue_release_req
 #define NGAP_PDUSESSION_RELEASE_COMMAND(mSGpTR)      (mSGpTR)->ittiMsg.ngap_pdusession_release_command
@@ -698,6 +699,37 @@ typedef struct {
   // Target to Source Transparent Container
   ngap_pdu_t target2source;
 } ngap_handover_request_ack_t;
+
+/* 9.3.4.10 3GPP TS 38.413 */
+typedef struct {
+  // QoS Flow to be Forwarded List
+  uint8_t nb_of_qos_flow;
+  pdusession_associate_qosflow_t qos_setup_list[QOSFLOW_MAX_VALUE];
+  // UL Forwarding UP TNL Information
+  uint32_t gtp_teid;
+  transport_layer_addr_t gNB_addr;
+} ho_command_transfer_t;
+
+typedef struct {
+  // PDU Session ID (M)
+  uint8_t pdusession_id;
+  // Handover Command Transfer (M)
+  ho_command_transfer_t ho_command_transfer;
+} pdusession_resource_handover_t;
+
+typedef struct {
+  // RAN UE NGAP ID
+  uint32_t gNB_ue_ngap_id;
+  // AMF UE NGAP ID
+  uint64_t amf_ue_ngap_id;
+  // Target to Source Transparent Container
+  ngap_pdu_t handoverCommand;
+  // Handover Type
+  ho_type_t handoverType;
+  // PDU Session Resource Handover List
+  uint8_t nb_of_pdusessions;
+  pdusession_resource_handover_t pdu_sessions[NGAP_MAX_PDU_SESSION];
+} ngap_handover_command_t;
 
 typedef struct ngap_ue_cap_info_ind_s {
   uint32_t  gNB_ue_ngap_id;