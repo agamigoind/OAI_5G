arandomdev/openairinterface5g

use-complex-type-in-lte-pucch

openair1/PHY/TOOLS/tools_defs.h
@@ -268,6 +268,10 @@ extern "C" {
         .i = a.r * b.i + a.i * b.r
     };
   }
+  __attribute__((always_inline)) inline cd_t cdMulConj(const cd_t a, const cd_t b)
+  {
+    return (cd_t){.r = a.r * b.r + a.i * b.i, .i = a.r * b.i - a.i * b.r};
+  }
 
   // On N complex numbers
   //   y.r += (x * alpha.r) >> 14

agamigoind/OAI_5G

openair1/PHY/TOOLS/tools_defs.h
@@ -267,6 +267,10 @@ extern "C" {
         .i = a.r * b.i + a.i * b.r
     };
   }
+  __attribute__((always_inline)) inline cd_t cdMulConj(const cd_t a, const cd_t b)
+  {
+    return (cd_t){.r = a.r * b.r + a.i * b.i, .i = a.i * b.r - a.r * b.i};
+  }
 
   // On N complex numbers
   //   y.r += (x * alpha.r) >> 14