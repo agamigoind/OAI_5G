arandomdev/openairinterface5g

hotfix for F. Mani's update to nr_feptx_prec handling of multiple beams

radio/fhi_72/oran_isolate.c
@@ -198,7 +198,7 @@ int trx_oran_ctlrecv(openair0_device *device, void *msg, ssize_t msg_len)
 void oran_fh_if4p5_south_in(RU_t *ru, int *frame, int *slot)
 {
   ru_info_t ru_info;
-  ru_info.nb_rx = ru->nb_rx;
+  ru_info.nb_rx = ru->nb_rx*ru->num_beams_period;
   ru_info.rxdataF = ru->common.rxdataF;
   ru_info.prach_buf = ru->prach_rxsigF[0]; // index: [prach_oca][ant_id]
 
@@ -252,7 +252,7 @@ void oran_fh_if4p5_south_out(RU_t *ru, int frame, int slot, uint64_t timestamp)
 {
   start_meas(&ru->tx_fhaul);
   ru_info_t ru_info;
-  ru_info.nb_tx = ru->nb_tx;
+  ru_info.nb_tx = ru->nb_tx*ru->num_beams_period;
   ru_info.txdataF_BF = ru->common.txdataF_BF;
   // printf("south_out:\tframe=%d\tslot=%d\ttimestamp=%ld\n",frame,slot,timestamp);
 

agamigoind/OAI_5G

radio/fhi_72/oran_isolate.c
@@ -208,7 +208,7 @@ int trx_oran_ctlrecv(openair0_device *device, void *msg, ssize_t msg_len)
 void oran_fh_if4p5_south_in(RU_t *ru, int *frame, int *slot)
 {
   ru_info_t ru_info;
-  ru_info.nb_rx = ru->nb_rx;
+  ru_info.nb_rx = ru->nb_rx * ru->num_beams_period;
   ru_info.rxdataF = ru->common.rxdataF;
   ru_info.prach_buf = ru->prach_rxsigF[0]; // index: [prach_oca][ant_id]
 
@@ -262,7 +262,7 @@ void oran_fh_if4p5_south_out(RU_t *ru, int frame, int slot, uint64_t timestamp)
 {
   start_meas(&ru->tx_fhaul);
   ru_info_t ru_info;
-  ru_info.nb_tx = ru->nb_tx;
+  ru_info.nb_tx = ru->nb_tx * ru->num_beams_period;
   ru_info.txdataF_BF = ru->common.txdataF_BF;
   // printf("south_out:\tframe=%d\tslot=%d\ttimestamp=%ld\n",frame,slot,timestamp);
 